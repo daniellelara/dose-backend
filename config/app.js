558b7cc5e6fa3710a97c2dd9a765b9c5b96634ce
daniellelara <daniellelaragourgey@gmail.com>
2016-04-26 12:57:42 +0100

    changed to http

--- config/app.js
+++ config/app.js
@@ -2,5 +2,5 @@ module.exports = {
   port: process.env.PORT || 3000,
   databaseUrl: process.env.MONGOLAB_URI || 'mongodb://localhost/dose-app',
   secret: 'spoonfullofsugarhelpsthemedicinegodown',
-  appUrl: process.env.NODE_ENV === 'production' ? 'https://wdi-dose.herokuapp.com' : 'http://localhost:8000/'
+  appUrl: process.env.NODE_ENV === 'production' ? 'wdi-dose.herokuapp.com' : 'http://localhost:8000/'
 }
